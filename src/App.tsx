785b0752679f1ae7d929020fa48e5cf9f4811b0b
Rikki Tissier <rtissier@gmail.com>
2023-05-03 22:40:38 -0400

    Testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,59 @@
 import HelpScout, { NOTIFICATION_TYPES } from "@helpscout/javascript-sdk";
-import { Button, DefaultStyle, Heading } from "@helpscout/ui-kit";
-import { useEffect, useState } from "react";
+import { Button, DefaultStyle, Heading, Icon } from "@helpscout/ui-kit";
+import { useEffect, useState, useRef } from "react";
+import { Octokit } from "@octokit/core";
+import DropList from "@hsds/drop-list";
 
 function App() {
-  const [userEmail, setUserEmail] = useState<string | undefined>(
-    "unknown user"
-  );
+	const [userEmail, setUserEmail] = useState<string | undefined>("unknown user");
+	const [issues, setIssues] = useState<any[]>([]); // TODO: type this as [Octokit.IssuesListForRepoResponseItem]
+	const octokit = useRef(new Octokit({ auth: process.env.ACCESS_TOKEN }));
 
-  useEffect(() => {
-    HelpScout.getApplicationContext().then(({ user }) =>
-      setUserEmail(user?.email)
-    );
-  }, []);
+	useEffect(() => {
+		const searchIssues = async () => {
+			return await octokit.current.request("GET /search/issues", {
+				q: "repo:rikkitissier/helpscout-example is:issue is:open",
+			});
+		};
 
-  function onClick() {
-    HelpScout.showNotification(
-      NOTIFICATION_TYPES.SUCCESS,
-      "Hello from the sidebar app"
-    );
-  }
+		searchIssues().then((response) => setIssues(response.data.items));
+	}, []);
 
-  return (
-    <div className="App">
-      <DefaultStyle />
-      <Heading level="h4">Hi, {userEmail}</Heading>
-      <br />
-      <Button size="sm" onClick={onClick}>
-        Click me
-      </Button>
-    </div>
-  );
+	useEffect(() => {
+		HelpScout.getApplicationContext().then(({ user }) => setUserEmail(user?.email));
+	}, []);
+
+	function onClick() {
+		HelpScout.showNotification(NOTIFICATION_TYPES.SUCCESS, "Hello from the sidebar app");
+	}
+
+	return (
+		<div className="App">
+			<DefaultStyle />
+
+			<DropList
+				toggler={<Button size="sm" />}
+				items={[
+					{ id: "1", value: "One" },
+					{ id: "2", value: "Two" },
+					{ id: "3", value: "Three" },
+				]}
+			/>
+
+			<Heading level="h4">Hi, {userEmail}</Heading>
+			<br />
+			<Button size="sm" onClick={onClick}>
+				<Icon name="plus" />
+			</Button>
+
+			{issues.map((issue) => (
+				<div key={issue.id}>
+					<h1>{issue.title}</h1>
+					<p>{issue.body}</p>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,16 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), !process.env.CODESANDBOX_HOST && basicSsl()],
+	define: {
+    "process.env": process.env,
+		// By default, Vite doesn't include shims for NodeJS/
+		// necessary for segment analytics lib to work
+		global: {},
+	},
+	resolve: {
+		alias: {
+			"node-fetch": "isomorphic-fetch",
+		},
+	},
+	plugins: [react(), !process.env.CODESANDBOX_HOST && basicSsl()],
 });
