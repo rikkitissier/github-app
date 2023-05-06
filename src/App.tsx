d7859065f965388aeb63b660343b3d2736abba00
Rikki <rtissier@gmail.com>
2023-05-06 03:31:44 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,82 @@
-import HelpScout, { NOTIFICATION_TYPES, ConfirmNotificationOptions } from "@helpscout/javascript-sdk";
+import HelpScout, {
+  NOTIFICATION_TYPES,
+  ConfirmNotificationOptions,
+} from "@helpscout/javascript-sdk";
 import { Button, DefaultStyle, Heading, Icon } from "@helpscout/ui-kit";
 import { useEffect, useState, useRef } from "react";
-import { Octokit, Repository } from "@octokit/core";
+import { Octokit } from "@octokit/core";
 import DropList from "@hsds/drop-list";
 
 interface Repository {
-	id: number;
-	full_name: string;
-	has_issues: boolean;
+  id: number;
+  full_name: string;
+  has_issues: boolean;
 }
 
 interface DropListItem {
-	id: string;
-	value: string;
+  id: string;
+  value: string;
 }
 
 function App() {
-	const [repos, setRepos] = useState<Repository[]>([]);
-	const octokit = useRef<Octokit>(new Octokit({ auth: import.meta.env.VITE_GH_ACCESS_TOKEN }));
+  const [repos, setRepos] = useState<Repository[]>([]);
+  const octokit = useRef<Octokit>(
+    new Octokit({ auth: import.meta.env.VITE_GH_ACCESS_TOKEN })
+  );
 
-	useEffect(() => {
-		const userRepos = async () => {
-			return await octokit.current.request("GET /user/repos");
-		};
+  useEffect(() => {
+    window.addEventListener("message", function onMessage(event) {
+      console.log(event.origin);
+    });
+  }, []);
 
-		userRepos().then((response) => {
-			const reposToList = response.data.filter((repo: Repository) => repo.has_issues);
-			setRepos(reposToList);
-		});
-	}, []);
+  useEffect(() => {
+    const userRepos = async () => {
+      return await octokit.current.request("GET /user/repos");
+    };
 
-	const handleOnSelectRepo = (selected: DropListItem) => {
+    userRepos().then((response) => {
+      const reposToList = response.data.filter(
+        (repo: Repository) => repo.has_issues
+      );
+      setRepos(reposToList);
+    });
+  }, []);
+
+  const handleOnSelectRepo = (selected: DropListItem) => {
     HelpScout.getConfirmNotificationConfirmed().then((confirmed) => {
-      console.log(confirmed)
-    })
+      console.log(confirmed);
+    });
 
-		HelpScout.showNotification(NOTIFICATION_TYPES.CONFIRM, "Create new issue?", {
-			body: `Are you sure you want to link this conversation to a new issue in <strong>${selected.value}</strong>?`,
-		} as ConfirmNotificationOptions);
-	};
+    HelpScout.showNotification(
+      NOTIFICATION_TYPES.CONFIRM,
+      "Create new issue?",
+      {
+        body: `Are you sure you want to link this conversation to a new issue in ${selected.value}?`,
+      } as ConfirmNotificationOptions
+    );
+  };
 
-	return (
-		<div className="App">
-			<DefaultStyle />
+  return (
+    <div className="App">
+      <DefaultStyle />
 
-			<DropList autoSetComboboxAt={10} inputPlaceholder="Choose repo..." toggler={<Button>Create an issue</Button>} items={repos.map((repo) => ({ id: repo.id.toString(), value: repo.full_name }))} onSelect={handleOnSelectRepo} />
-		</div>
-	);
+      <DropList
+        autoSetComboboxAt={10}
+        inputPlaceholder="Choose repo..."
+        toggler={
+          <Button style={{ width: "100%" }} theme="grey" styled="outlined">
+            Create an issue
+          </Button>
+        }
+        items={repos.map((repo) => ({
+          id: repo.id.toString(),
+          value: repo.full_name,
+        }))}
+        onSelect={handleOnSelectRepo}
+      />
+    </div>
+  );
 }
 
 export default App;
