7d22c86d8f7b8d0242df479927040725bef8b776
Deiva Magalhaes <deivamagalhaes@helpscout.com>
2023-06-20 16:07:01 -0700

    Update with changes from the app-template

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
+import mkcert from 'vite-plugin-mkcert'
 import react from "@vitejs/plugin-react";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,5 +15,5 @@ export default defineConfig({
 			"node-fetch": "isomorphic-fetch",
 		},
 	},
-	plugins: [react(), !process.env.CODESANDBOX_HOST && basicSsl()],
+	plugins: [react(), !process.env.CODESANDBOX_HOST && mkcert()],
 });
